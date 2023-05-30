86b4e38d672365cdd6573918ebf1394008e18c7b
TJWGMK5G <87062165+TJWGMK5G@users.noreply.github.com>
2023-05-30 15:10:47 +0300

    123
    123

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,11 +4,11 @@ new Swiper('.house-slider' , {
         nextEl: 'swiper-button-next',
         prevEl: 'swiper-button-prev',
     },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    },
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    //     dynamicBullets: true,
+    // },
         autoHeight: true,
         slidesPerView: 2,
         spaceBetween: -350,
