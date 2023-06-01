6897f2ff2dd910de04141b58867575501cf43164
TJWGMK5G <87062165+TJWGMK5G@users.noreply.github.com>
2023-06-01 13:53:46 +0300

    123
    213

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,3 +16,23 @@ new Swiper('.house-slider' , {
     
 });
 
+
+new Swiper('.main-review__swiper', {
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        },
+        autoHeight: true,
+        // slidesPerView: 1,
+        spaceBetween: 250,
+        loop: true,
+        slidesPerGroup: 1,
+        centeredSlides: true,
+        loop: true,
+        // autoplay: {
+        //     delay: 4000,
+        //     disavleOnInterection: true,
+        // },
+        speed: 1000,
+})
